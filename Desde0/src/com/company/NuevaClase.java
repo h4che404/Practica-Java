25e03c32dd8cc7d3ee130e714fa6031a4fe2a026
Juan Cruz Elias Martin <108189589+h4che404@users.noreply.github.com>
2023-05-30 18:19:41 -0300

    Add files via upload

---
+++ Desde0/src/com/company/NuevaClase.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class NuevaClase {
+
+    byte var = 1;
+    long var2 = 1000000000;
+    short var1 = 10000;
+    int var3 = 1000000;
+    float var4 = 12.3f;
+    double var5 = 12.42132132421213421331d;
+    boolean verdadero = true;
+    boolean falso = false;
+
+    String palabra = "hola mundo";
+    char caracter = 'a';
+
+}

701277d6dbbaf54b6f25c993036d7f6839d33bdf
Juan Cruz Elias Martin <108189589+h4che404@users.noreply.github.com>
2023-05-30 18:25:15 -0300

    Add files via upload

--- Desde0/src/com/company/NuevaClase.java
+++ Desde0/src/com/company/NuevaClase.java
@@ -2,16 +2,31 @@ package com.company;
 
 public class NuevaClase {
 
-    byte var = 1;
-    long var2 = 1000000000;
-    short var1 = 10000;
-    int var3 = 1000000;
-    float var4 = 12.3f;
-    double var5 = 12.42132132421213421331d;
-    boolean verdadero = true;
-    boolean falso = false;
+    public static byte var = 1;
+    public static long var2 = 1000000000;
+    public static short var1 = 10000;
+    public static int var3 = 1000000;
+    public static float var4 = 12.3f;
+    public static double var5 = 12.42132132421213421331d;
+    public static boolean verdadero = true;
+    public static boolean falso = false;
+    public static String palabra = "hola mundo";
+    public static char caracter = 'a';
 
-    String palabra = "hola mundo";
-    char caracter = 'a';
+    public static class Main {
+        public static void main(String[] args) {
 
+            System.out.println(var);
+            System.out.println(var1);
+            System.out.println(var2);
+            System.out.println(var3);
+            System.out.println(var4);
+            System.out.println(var5);
+            System.out.println(verdadero);
+            System.out.println(falso);
+            System.out.println(palabra);
+            System.out.println(caracter);
+
+        }
+    }
 }

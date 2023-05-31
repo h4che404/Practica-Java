69db991a20762ad4538febc7417f94e0d04fc484
Juan Cruz Elias Martin <108189589+h4che404@users.noreply.github.com>
2023-05-31 17:11:07 -0300

    Add files via upload

---
+++ Bucles/src/Main.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        String[] var = {"juan", "Ariel", "adasda", "adasdsa"};
+
+        for (String i: var) {
+            System.out.println(i);
+        }
+    }
+}
